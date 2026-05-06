653b477bc04fd506c30e165caa7850b2446b5254
manuelbenitez <manu.benitez1@hotmail.com>
2026-05-06 17:47:13 +0200

    chore(deps): enable generated image tooling

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default withNextIntl(nextConfig);
